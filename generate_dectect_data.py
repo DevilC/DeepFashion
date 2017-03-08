d9bd581efe64fb0ffed87d19ae275f1f4da38951
DevilC <lwc2015@yeah.net>
2017-03-08 15:34:21 +0800

    edit save

--- generate_dectect_data.py
+++ generate_dectect_data.py
@@ -4,29 +4,31 @@ import Image
 import time
 import numpy as np
 
-positive_sample = []
-negetive_sample = []
+positive_sample = [[]]
+negetive_sample = [[]]
 
 start_time = time.clock()
 n = 0
 for i in range(209222):
-    positive_sample.append([])
-    negetive_sample.append([])
     img = Image.open(data.train_data[i])
     candidata = select.my_selective_search(img)
     #print candidata.__len__()
     target = data.img_bbox[data.train_data[i]]
     print n
+    p_temp = ''
+    n_temp = ''
     for c in candidata:
         IOU = select.get_IOU(target, c)
         if IOU >0.6:
-            positive_sample[i].append(c)
+            p_temp += ','.join(str(j)for j in c)
         if IOU <0.3:
-            negetive_sample[i].append(c)
+            n_temp += ','.join(str(j) for j in c)
+    positive_sample.append(p_temp)
+    negetive_sample.append(n_temp)
     n+=1
 
 np.save('./detect_sample/positive', np.array(positive_sample))
-np.save('./detect_sample/positive', np.array(negetive_sample))
+np.save('./detect_sample/negetive', np.array(negetive_sample))
 end_time = time.clock()
 print 'generate_time:', end_time-start_time
 
