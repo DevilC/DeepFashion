7bd71a682cf5924dce958bebacd1a1f769a7ea0a
DevilC <lwc2015@yeah.net>
2017-03-08 17:37:16 +0800

    edit generate

--- generate_dectect_data.py
+++ generate_dectect_data.py
@@ -4,29 +4,37 @@ import Image
 import time
 import numpy as np
 
-positive_sample = [[]]
-negetive_sample = [[]]
+positive_sample = []
+negetive_sample = []
+
+tag = [0 for i in range(209222)]
 
 start_time = time.clock()
-n = 0
-for i in range(209222):
-    img = Image.open(data.train_data[i])
-    candidata = select.my_selective_search(img)
-    #print candidata.__len__()
-    target = data.img_bbox[data.train_data[i]]
-    print n
-    p_temp = ''
-    n_temp = ''
-    for c in candidata:
-        IOU = select.get_IOU(target, c)
-        if IOU >0.6:
-            p_temp += ','.join(str(j)for j in c)
-        if IOU <0.3:
-            n_temp += ','.join(str(j) for j in c)
-    positive_sample.append(p_temp)
-    negetive_sample.append(n_temp)
-    n+=1
+i = 0
+while i<50000:
+    this_i = np.random.randint(0,209222)
+    if 1 == tag[this_i]:
+        continue
+    else:
+        tag[this_i]=1
+        img = Image.open(data.train_data[this_i])
+        candidata = select.my_selective_search(img)
+        #print candidata.__len__()
+        target = data.img_bbox[data.train_data[this_i]]
+        p_temp = str(this_i)+' '
+        n_temp = str(this_i)+' '
+        for c in candidata:
+            IOU = select.get_IOU(target, c)
+            if IOU >0.6:
+                p_temp += ','.join(str(j)for j in c)+' '
+            if IOU <0.3:
+                n_temp += ','.join(str(j) for j in c)+' '
+        positive_sample.append(p_temp)
+        negetive_sample.append(n_temp)
+        print i, this_i
+        i+=1
 
+print positive_sample
 np.save('./detect_sample/positive', np.array(positive_sample))
 np.save('./detect_sample/negetive', np.array(negetive_sample))
 end_time = time.clock()
